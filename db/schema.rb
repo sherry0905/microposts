3fa5e5e88fb473588b8d0dfe42dfb0b2248681d3
katsumi saito <katsumi-saitou@incgrow.co.jp>
2016-10-07 15:19:43 +0000

    profile add

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003123140) do
+ActiveRecord::Schema.define(version: 20161007123620) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20161003123140) do
     t.datetime "updated_at",      null: false
     t.string   "location"
     t.integer  "age"
+    t.string   "profile"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
